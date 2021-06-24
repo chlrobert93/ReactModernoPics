060747589981af2cecbf621dfe945850bec87f7b
Eder <chlrobert93@gmail.com>
2021-06-24 12:27:10 -0500

    Creacción de controladores de eventos

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+//Cada vez que alguien cambia el texto dentro de nuestra entrada
+
 
 //Coponente basado en clase
 //Para utilisar state
 class SearchBar extends React.Component {
+
+//Definir un nuevo método
+   onInputChange(event){
+       //Esto contendra el texto que el usuario acaba de agregar a nuestra entrada
+      console.log(event.target.value);
+
+   }
+
+   onInputClick(){
+       console.log('Input was clicked');
+   }
+
     render(){
         
         return(
@@ -11,7 +25,7 @@ class SearchBar extends React.Component {
             <form className="ui form">
                 <div className="field">
                   <label>Image Search</label>
-                  <input type="text" />
+                  <input type="text" onClick={this.onInputClick} onChange={this.onInputChange}/>
                </div>
             </form>
         </div>
