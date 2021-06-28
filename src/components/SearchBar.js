e5f2b726bf85e80425f2ae412b235195a64ac6ce
Eder <chlrobert93@gmail.com>
2021-06-28 11:28:29 -0500

    Elementos no controlados freante a elementos controlados  y recarga

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -16,16 +16,27 @@ class SearchBar extends React.Component {
 
    //Objec State
     state = { term: ''};
+     
+    onFormSubmit(event){
+        //Intente enviar el formulario automáticamentey en el proceso actualizen la página
+        event.preventDefault();
+            console.log(this.state.term);
 
+    }
 
+            /*En el instante en que el susuario escribe que ingresa esta función, se llama 
+              extraemos el valor actual de la entrada, actualiamos nuestro componente al llamar al state establecido
+              Cada vez que llamamos al sate establecido, el compienente se procesará automaticamente, por lo que el método
+              de procesamiento aqui se llama por segunda vez en el instatne en que el usuario escribe algo allí
+            */
     render(){
         
         return(
         <div className="ui segment">
-            <form className="ui form">
+            <form onSubmit={this.onFormSubmit} className="ui form">
                 <div className="field">
-                  <label>Image Search</label>
-                  <input type="text" value={this.state.term} onChange={(e) => this.setState({ term: e.target.value})} /*onChange={this.onInputChange}*//>
+                  <label>Image Search</label>      
+                  <input type="text" value={this.state.term} onChange={(e) => this.setState({ term: e.target.value.toUpperCase()})} /*onChange={this.onInputChange}*//>
                </div> 
             </form>
         </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './components/App';
 import "semantic-ui-css/semantic.min.css";
 
 
-
+if (module.hot) {
+    module.hot.accept();
+  }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
